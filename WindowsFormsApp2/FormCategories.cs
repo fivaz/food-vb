fa0ac8e0e7ebb0472fefd9ed89ebd4d46624e6f8
Stefane Fivaz <s.a.fivaz@gmail.com>
2020-03-22 23:32:03 +0100

    formCategories done

---
+++ WindowsFormsApp2/FormCategories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class frmCategories : Form
+    {
+        public frmCategories()
+        {
+            InitializeComponent();
+        }
+
+        private void FormCategories_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new FormAddIngredient().Show();
+        }
+    }
+}
